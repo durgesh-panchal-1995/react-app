d1033c4b74e64e87c6567e4a4cb1378fbaad67c5
Durgesh Panchal <durgeshpanchal1995@gmail.com>
2026-04-14 21:38:14 +0530

    project files
    All the project files and folders uploaded

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { FlashcardQuiz } from './components/flashQuiz'
+function App() {
+  return (
+    <div className="App">
+     <FlashcardQuiz />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/flashcards.js
@@ -0,0 +1,28 @@
+// Sample flashcard data - you can add more questions/answers here
+export const flashcardsData = [
+  {
+    id: 1,
+    question: "What is the capital of France?",
+    answer: "Paris"
+  },
+  {
+    id: 2,
+    question: "What is 2 + 2?",
+    answer: "4"
+  },
+  {
+    id: 3,
+    question: "Who wrote Romeo and Juliet?",
+    answer: "William Shakespeare"
+  },
+  {
+    id: 4,
+    question: "What is the largest planet in our solar system?",
+    answer: "Jupiter"
+  },
+  {
+    id: 5,
+    question: "What is the chemical symbol for Gold?",
+    answer: "Au"
+  }
+];
